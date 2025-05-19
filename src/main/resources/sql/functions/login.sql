7eb69fb029f417c99c33df97a4995f26b22741db
Perdomo Cardenas Juan Manuel <perdomocardenas18@gmail.com>
2025-05-18 22:01:39 -0500

    fn_login listo

--- src/main/resources/sql/functions/login.sql
+++ src/main/resources/sql/functions/login.sql
@@ -1,55 +1,41 @@
-CREATE OR REPLACE FUNCTION fn_login(
-    p_username IN VARCHAR2,
-    p_password IN VARCHAR2,
-    tipo IN CHAR
+create or replace FUNCTION fn_login(
+p_username IN VARCHAR2,
+p_password IN VARCHAR2,
+tipo IN NUMBER
+)RETURN INTEGER IS
+flag INTEGER := 0;
+v_id estudiante.id%type;
 
-) RETURN BOOLEAN IS
-    flag BOOLEAN;
-
-DECLARE
-    flag BOOLEAN:=FALSE;
-    v_result estudiante%ROWTYPE;
-    v_result docente%ROWTYPE;
 BEGIN
 
 IF tipo=1 THEN
-SELECT
-    'ESTUDIANTE' AS tipo_usuario,
-    id,
-    nombre,
-    nombreUsuario AS username
-FROM
-    estudiante
-WHERE
-        nombreUsuario = p_username
-  AND contrasena = p_password;
 
-flag:= TRUE;
+SELECT id
+INTO v_id
+FROM estudiante
+WHERE nombreusuario = p_username AND contrasena = p_password;
+
+flag := v_id;
 
 ELSE
+SELECT id
+INTO v_id
+FROM docente
+WHERE nombreusuario = p_username AND contrasena = p_password;
 
-SELECT
-    'DOCENTE' AS tipo_usuario,
-    id,
-    nombre,
-    nombreUsuario AS username
-FROM
-    docente
-WHERE
-        nombreUsuario = p_username
-  AND contrasena = p_password;
+flag := v_id;
 
-flag:= TRUE;
 END IF;
 
 RETURN flag;
 
 EXCEPTION
- WHEN NO_DATA_FOUND THEN
-    flag := FALSE;
- WHEN TOO_MANY_ROWS THEN
-    flag := FALSE;
-END;
+WHEN NO_DATA_FOUND THEN
+flag := 0;
+RETURN flag;
+
+WHEN TOO_MANY_ROWS THEN
+flag := 0;
+RETURN flag;
 
 END fn_login;
-/
